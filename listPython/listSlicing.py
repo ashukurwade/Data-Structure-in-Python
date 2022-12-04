6d5b8c3dff6d7206550e047cac9b0afe3b6b791b
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-04 23:15:58 +0530

    We can access a range of items in a list by using the slicing operator :

---
+++ listPython/listSlicing.py
@@ -0,0 +1,12 @@
+# List slicing in Python
+
+my_list = ("a", "s", "h", "u", "t", "o", "s", "h")
+
+# elements from index 2 to index 4
+print(my_list[0:4])
+
+# elements from index 5 to end
+print(my_list[2:])
+
+# elements beginning to end
+print(my_list[:])
