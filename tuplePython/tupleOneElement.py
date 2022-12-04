00675e7f2df4bd956dff7818d04bf5e799d0b9a5
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-04 22:57:39 +0530

    creating a tuple with one element is a bit tricky. Having one element within parentheses is not enough.

---
+++ tuplePython/tupleOneElement.py
@@ -0,0 +1,13 @@
+var1 = ("Hello") # string
+var2 = ("Hello",) # tuple
+
+var1 = ("hello")
+print(type(var1))  # <class 'str'>
+
+# Creating a tuple having one element
+var2 = ("hello",)
+print(type(var2))
+
+# Parentheses is optional
+var3 = "hello",
+print(type(var3))
