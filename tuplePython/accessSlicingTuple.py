14df64f5de1d08770579f7af890a8baf19e7cf24
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-04 22:56:13 +0530

    access a range of items in a tuple by using the slicing operator colon :.

---
+++ tuplePython/accessSlicingTuple.py
@@ -0,0 +1,14 @@
+# accessing tuple elements using slicing
+my_tuple = ("a", "s", "h", "u", "t", "o", "s", "h")
+
+# elements 2nd to 4th index
+print(my_tuple[1:4])
+
+# elements beginning to 2nd
+print(my_tuple[:-7]) 
+
+# elements 8th to end
+print(my_tuple[7:]) 
+
+# elements beginning to end
+print(my_tuple[:]) 
