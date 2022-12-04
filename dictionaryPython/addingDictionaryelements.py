d1291323dfe3259e4956783ebf9ee1d42bd66de2
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-04 22:36:43 +0530

    Dictionaries are mutable. We can add new items using an assignment operator.

---
+++ dictionaryPython/addingDictionaryelements.py
@@ -0,0 +1,12 @@
+# Changing and adding Dictionary Elements
+my_dict = {'name': 'ashu', 'age': 22}
+
+# update value
+my_dict['age'] = 23
+
+print(my_dict)
+
+# add item
+my_dict['address'] = 'Amravati'
+
+print(my_dict)
